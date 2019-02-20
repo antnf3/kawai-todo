1d4c81453488a867a5cfa76cb988b7202552affe
antnf3 <antnf3@gmail.com>
2019-02-21 07:31:55 +0900

    다자인 추가 및 복구

--- App.js
+++ App.js
@@ -1,55 +1,83 @@
 import React from 'react';
-import { StyleSheet, Text, View, StatusBar, Dimensions, Platform } from 'react-native';
-import { TextInput } from 'react-native-gesture-handler';
+import {
+	StyleSheet,
+	Text,
+	View,
+	StatusBar,
+	TextInput,
+	Dimensions,
+	Platform,
+	ScrollView,
+	AsyncStorage,
+} from 'react-native';
+import { AppLoading } from 'expo';
+import uuidv1 from 'uuid/v1';
 
-const { height, width} = Dimensions.get("window");
+const { height, width } = Dimensions.get('window');
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <StatusBar barStyle="light-content" />
-        <Text style={styles.title}>Kawai To Do</Text>
-        <View style={styles.card}>
-          <TextInput style={styles.input} placeholder={"New To Do"}/>
-        </View>
-      </View>
-    );
-  }
+	state = {
+		newToDo: '',
+		loadedToDos: false,
+		toDos: {},
+	};
+	componentDidMount = () => {
+		//this._loadToDos();
+	};
+	render() {
+		return (
+			<View style={styles.container}>
+				<StatusBar barStyle="light-content" />
+				<Text style={styles.title}>Kawai To Do</Text>
+				<View style={styles.card}>
+					<TextInput style={styles.input} placeholder={'New To Do'} />
+				</View>
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#F23657',
-    alignItems: 'center'
-  },
-  title: {
-    color: 'white',
-    fontSize: 30,
-    marginTop: 50,
-    fontWeight: '200',
-    marginBottom: 30
-  },
-  card: {
-    backgroundColor: 'white',
-    flex: 1,
-    width: width - 25,
-    borderTopLeftRadius: 10,
-    borderTopRightRadius: 10,
-    ...Platform.select({
-      ios: {
-        shadowColor: "rgb(50, 50, 50)",
-        shadowOpacity: 0.5,
-        shadowRadius: 5,
-        shadowOffset: {
-          height: -1,
-          width: 0
-        }
-      },
-      android: {
-        elevation: 3
-      }
-    })
-  }
+	container: {
+		flex: 1,
+		backgroundColor: '#F23657',
+		alignItems: 'center',
+	},
+	title: {
+		color: 'white',
+		fontSize: 30,
+		marginTop: 50,
+		fontWeight: '200',
+		marginBottom: 30,
+	},
+	card: {
+		backgroundColor: 'white',
+		flex: 1,
+		width: width - 25,
+		borderTopLeftRadius: 10,
+		borderTopRightRadius: 10,
+		...Platform.select({
+			ios: {
+				shadowColor: 'rgb(50, 50, 50)',
+				shadowOpacity: 0.5,
+				shadowRadius: 5,
+				shadowOffset: {
+					height: -1,
+					width: 0,
+				},
+			},
+			android: {
+				elevation: 3,
+			},
+		}),
+	},
+	input: {
+		padding: 20,
+		borderBottomColor: '#bbb',
+		borderBottomWidth: 1,
+		fontSize: 25,
+	},
+	toDo: {
+		alignItems: 'center',
+	},
 });
